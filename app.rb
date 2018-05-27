e72fa724777f7f223d9a4159fafb4dd828f82411
Ally Netzel <netzelally@gmail.com>
2018-05-27 20:42:17 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,12 +1,12 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
-
+	
 	get '/' do
-	erb :index
-end
-
-get "/info" do
-	erb :info
-end
+		erb :index
+	end
+	
+	get "/info" do
+		erb :info
+	end
 end
